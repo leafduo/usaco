f7644fdcc30a970aeabab93b5063b8b2cea32f67
leafduo <leafduo@gmail.com>
2009-03-18 22:27:12 +0800

    mar09 iou

--- contest/mar09/iou.cpp
+++ contest/mar09/iou.cpp
@@ -1,5 +1,5 @@
 /*ID:leafduo1
-  PROG:iou
+  TASK:iou
   LANG:C++
 */
 
@@ -10,62 +10,23 @@ using namespace std;
 ifstream fin("iou.in");
 ofstream fout("iou.out");
 
-const int maxn = 100000;
-
-int f[maxn + 1];
-int N;
-
-void input()
-{
-	fin >> N;
-	for (int i=1;i<=N;++i)
-		fin>>f[i];
-}
-
-int move()
-{
-	int now = 0, pay = 0, dis = 0, money = 0;
-	while(pay <= N) {
-		if (f[now]>0) {
-                        money += f[now];
-                        f[now] = 0;
-                        ++now;
-                        ++pay;
-                        ++dis;
-                }
-                else if (money >= -f[now]) {
-                        money += f[now];
-                        f[now] = 0;
-                        ++now;
-                        ++pay;
-                        ++dis;
-                }	
-                else {
-                        int wait = now;
-                        while (money<-f[wait]) {
-                                ++dis;
-                                ++now;
-                                if (f[now]>0)
-                                {
-                                        money += f[now];
-                                        f[now] = 0;
-                                }
-                        }
-
-                        money += f[wait];
-                        f[wait] = 0;
-                        dis += now - wait;
-                        now = wait;
-                        pay = now;
-                }
-        }
-        return --dis;
-}
-
 int main()
 {
-        input();
-        fout<< move() <<endl;
+        int N;
+        fin >> N;
+        int money = 0, pay = 0;
+        int dis = N;
+        for (int i = 1; i <= N; ++i) {
+                int now;
+                fin >> now;
+                money += now;
+                if (money < 0 && 0 == pay)
+                        pay = i;
+                else if (money > 0 && 0 != pay) {
+                        dis += (i - pay) * 2;
+                        pay = 0;
+                }
+        }
+        fout << dis << endl;
         return 0;
 }
-
